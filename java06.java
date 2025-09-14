9ed854e82db8584343326e2e3749f16a4523a41f
Atulchavan-2003 <atulch20200@gmail.com>
2025-09-14 11:11:28 +0530

    second file edited

--- java06.java
+++ java06.java
@@ -1,35 +1,40 @@
-class date{
-       int day,month,year;
+class date {
+	int day, month, year;
 	String dow;
-	void setday(int a){
-	      this.day=a;
-	} // setday end here 
 
-        void setmonth(int a){
-	      this.month=a;
-	}// month end here 
+	void setday(int a) {
+		this.day = a;
+	} // setday end here
 
-	void setyear(int a){
-	      this.year=a;
-	} 
+	void setmonth(int a) {
+		this.month = a;
+	}// month end here
 
-	void setdow(String str){
-	     dow = str;
-	} // setday end here 
+	void setyear(int a) {
+		this.year = a;
+	}
 
-}//date class end here 
+	void setdow(String str) {
+		dow = str;
+	} // setday end here
+
+}// date class end here
+
+class atul {
+	// hello atul
+}
 
 class test {
-	public static void main(String args[]){
-		
-		date d1 = new date();                
-                d1.setday(3);
+	public static void main(String args[]) {
+
+		date d1 = new date();
+		d1.setday(3);
 		d1.setmonth(8);
 		d1.setyear(2003);
 		d1.setdow("wed");
-              
+
 		System.out.println(d1.day);
- 		System.out.println(d1.month);
+		System.out.println(d1.month);
 		System.out.println(d1.year);
 
 		System.out.println(d1.dow);
--- salesmanager.java
+++ salesmanager.java
@@ -1,39 +1,46 @@
 import java.util.Scanner;
+
 class SalesManager {
     int id;
+    int bonus;
     String name;
     double salary;
     double target;
     double incentive;
-     
-	SalesManager(){
-	    this.name = "atul ";
-	    this.salary = 500000;
-	    this.target = 1000000;
-	    this.incentive = 5000;
-        }
 
-	SalesManager(int i,String n,double s, double t ,double inc){
-	    this.id = i;
-	    this.name = n;
-	    this.salary = s;
-	    this.target = t;
-	    this.incentive =inc;
-        }
+    SalesManager() {
+        this.name = "atul ";
+        this.salary = 500000;
+        this.target = 1000000;
+        this.incentive = 5000;
+    }
+
+    SalesManager(int i, String n, double s, double t, double inc) {
+        this.id = i;
+        this.name = n;
+        this.salary = s;
+        this.target = t;
+        this.incentive = inc;
+        this.bonus = i;
+    }
 
     // Setters
     void setId(int i) {
         id = i;
     }
+
     void setName(String n) {
         name = n;
     }
+
     void setSalary(double s) {
         salary = s;
     }
+
     void setTarget(double t) {
         target = t;
     }
+
     void setIncentive(double inc) {
         incentive = inc;
     }
@@ -42,15 +49,19 @@ class SalesManager {
     int getId() {
         return id;
     }
+
     String getName() {
         return name;
     }
+
     double getSalary() {
         return salary;
     }
+
     double getTarget() {
         return target;
     }
+
     double getIncentive() {
         return incentive;
     }
@@ -67,33 +78,32 @@ class SalesManager {
 
 class test {
     public static void main(String args[]) {
-	Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
         SalesManager sm1 = new SalesManager();
-	System.out.println("Enter the id");
-	int id = sc.nextInt();
+        System.out.println("Enter the id");
+        int id = sc.nextInt();
         sc.nextLine();
-	System.out.println("Enter the name");
-	String name = sc.nextLine();
-	System.out.println("Enter the salary");
-	double sal = sc.nextDouble();
- 	System.out.println("Enter the target");
-	double tar = sc.nextDouble();
-	System.out.println("Enter the incentive");
-	double incen = sc.nextDouble();
-        SalesManager sm2 = new SalesManager(id,name,sal,tar,incen);
-        
-	sm2.display();
-	
+        System.out.println("Enter the name");
+        String name = sc.nextLine();
+        System.out.println("Enter the salary");
+        double sal = sc.nextDouble();
+        System.out.println("Enter the target");
+        double tar = sc.nextDouble();
+        System.out.println("Enter the incentive");
+        double incen = sc.nextDouble();
+        SalesManager sm2 = new SalesManager(id, name, sal, tar, incen);
+
+        sm2.display();
+
         sm1.setId(101);
         sm1.setName("Atul");
         sm1.setSalary(60000);
         sm1.setTarget(100000);
         sm1.setIncentive(10000);
 
-        sm1.display();   
-	sm2.display();   
-          
-        
-      }
+        sm1.display();
+        sm2.display();
+
+    }
 }
